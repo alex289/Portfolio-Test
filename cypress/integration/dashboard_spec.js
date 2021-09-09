3aa52f9222cbe4f2542a9bda4e94e27cafcddc3d
Alex289 <mccr4fthd@gmail.com>
2021-09-09 15:01:12 +0200

    Fix dark light mode versions

--- cypress/integration/dashboard_spec.js
+++ cypress/integration/dashboard_spec.js
@@ -12,18 +12,29 @@ describe('Dashboard page', () => {
   });
 
   it('Dark/Light mode should work', () => {
-    cy.get('html.dark').should('exist');
+    cy.get('html').then((item) => {
+      if (item.hasClass('dark')) {
+        // Click twice to handle next-themes bug
+        cy.get(`button[aria-label="Toggle Dark Mode"]`)
+          .trigger('mouseover')
+          .click();
 
-    // Click twice to handle next-themes bug
-    cy.get(`button[aria-label="Toggle Dark Mode"]`)
-      .trigger('mouseover')
-      .click();
+        cy.get(`button[aria-label="Toggle Dark Mode"]`)
+          .trigger('mouseover')
+          .click();
+        cy.get('html.light').should('exist');
+      } else if (item.hasClass('light')) {
+        // Click twice to handle next-themes bug
+        cy.get(`button[aria-label="Toggle Dark Mode"]`)
+          .trigger('mouseover')
+          .click();
 
-    cy.get(`button[aria-label="Toggle Dark Mode"]`)
-      .trigger('mouseover')
-      .click();
-
-    cy.get('html.light').should('exist');
+        cy.get(`button[aria-label="Toggle Dark Mode"]`)
+          .trigger('mouseover')
+          .click();
+        cy.get('html.dark').should('exist');
+      }
+    });
   });
 
   it('Should have data', () => {
