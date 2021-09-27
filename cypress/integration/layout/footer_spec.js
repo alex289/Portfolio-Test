1b9a03fff45fb2f87db0c847cef22da4be3e7463
Alex289 <mccr4fthd@gmail.com>
2021-09-27 10:42:14 +0200

    Move link test to end

--- cypress/integration/layout/footer_spec.js
+++ cypress/integration/layout/footer_spec.js
@@ -21,9 +21,6 @@ describe('Footer component', () => {
     cy.get('footer').get('a').contains('Projects').click();
     cy.get('h3').contains('Projects').should('be.visible');
 
-    cy.get('footer').get('a').contains('Dashboard').click();
-    cy.url().should('eq', 'https://alexanderkonietzko.vercel.app/dashboard');
-
     cy.get('footer')
       .get('a')
       .contains('Portfolio')
@@ -50,5 +47,8 @@ describe('Footer component', () => {
         'include',
         'https://github.com/Alex289/Alex289/issues/new?assignees=Alex289&labels=Contact&template=contact-template.md&title=%5BContact%5D+Your-title-here'
       );
+
+    cy.get('footer').get('a').contains('Dashboard').click();
+    cy.url().should('eq', 'https://alexanderkonietzko.vercel.app/dashboard');
   });
 });
