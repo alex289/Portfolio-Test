89d37d023d6e25262249d04e62051d6eae45840b
Alex289 <mccr4fthd@gmail.com>
2021-12-20 21:21:24 +0100

    damn idk

--- cypress/integration/mobile_view_spec.js
+++ cypress/integration/mobile_view_spec.js
@@ -37,11 +37,11 @@ describe('Mobile view', () => {
   it('Dark/Light mode should work', () => {
     cy.get('html').then((item) => {
       if (item.hasClass('dark')) {
-        cy.get('#dark-mode-toggle').click().click();
+        cy.get('#dark-mode-toggle').click();
 
         cy.get('html.light').should('exist');
       } else if (item.hasClass('light')) {
-        cy.get('#dark-mode-toggle').click().click();
+        cy.get('#dark-mode-toggle').click();
 
         cy.get('html.dark').should('exist');
       }
