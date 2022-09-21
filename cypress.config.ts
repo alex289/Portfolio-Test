3d8d75a71e80aad51150f5d58a3436496216ff3d
Alex289 <mccr4fthd@gmail.com>
2022-09-21 15:23:32 +0200

    chore: Update deps

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,14 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   videoUploadOnPasses: false,
+  retries: { runMode: 3, openMode: 3 },
   e2e: {
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.ts').default(on, config)
+      return require('./cypress/plugins/index.ts').default(on, config);
     },
     baseUrl: 'https://alexanderkonietzko.vercel.app',
+    experimentalStudio: true,
   },
-})
+});
