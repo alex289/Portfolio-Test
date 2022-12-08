aa49b868597d7e10b1e914db37067c7d37b0bb5e
alex289 <mccr4fthd@gmail.com>
2022-12-08 11:20:05 +0100

    chore: Update deps

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,10 +5,9 @@ export default defineConfig({
   videoUploadOnPasses: false,
   retries: { runMode: 3, openMode: 3 },
   e2e: {
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.ts').default(on, config);
-    },
     baseUrl: 'https://alexanderkonietzko.vercel.app',
     experimentalStudio: true,
+    experimentalRunAllSpecs: true,
+    testIsolation: false,
   },
 });
